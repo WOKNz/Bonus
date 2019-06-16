cedfede648587b81d593a080a7a551f8f8197c09
Sasha <none>
2019-06-16 15:45:36 +0300

    Working version + Comments

--- Bonus.py
+++ Bonus.py
@@ -1,5 +1,6 @@
-import matplotlib.pyplot as plt
 import math
+import matplotlib.pyplot as plt
+
 
 class Point:
     def __init__(self, x, y):
@@ -29,6 +30,12 @@ class Polygon:
         return edge_list
 
     def contains(self, point):
+
+        def iscw(v1,v2):  # CW check
+            if v1.x*v2.y-v2.x*v1.y >= 0:
+                return True
+            else:
+                return  False
         """
         return whether the point inside or outside the polygon
 
@@ -40,6 +47,28 @@ class Polygon:
             this function is empty. needs implementation.
 
         """
+        temp_points = []
+        for vertex in points:
+            temp_points.append(Point(vertex.x - point.x, vertex.y - point.y))
+        if not (temp_points[-1].x == temp_points[0].x and temp_points[-1].y == temp_points[0].y):
+            temp_points.append(Point(temp_points[0].x, temp_points[0].y))
+        angle_sum = 0
+        for i in range(0, len(temp_points) - 1):  # Checking angle between 2 vectors
+            if iscw(temp_points[i], temp_points[i+1]):  # Adding angle case
+                angle_sum += math.acos(
+                    (temp_points[i].x * temp_points[i + 1].x + temp_points[i].y * temp_points[i + 1].y) /
+                    (math.sqrt(temp_points[i].x ** 2 + temp_points[i].y ** 2) * math.sqrt(
+                        (temp_points[i + 1].x) ** 2 + (temp_points[i + 1].y) ** 2)))
+            else:  # Adding angle case
+                angle_sum -= math.acos(
+                    (temp_points[i].x * temp_points[i + 1].x + temp_points[i].y * temp_points[i + 1].y) /
+                    (math.sqrt(temp_points[i].x ** 2 + temp_points[i].y ** 2) * math.sqrt(
+                        temp_points[i + 1].x ** 2 + temp_points[i + 1].y ** 2)))
+            # print('    i='+str(i)+','+str(angle_sum))
+        if (angle_sum/(2*math.pi)).is_integer():  # Checking for Rotations
+            return True
+        else:
+            return False
 
 
 if __name__ == "__main__":
@@ -91,5 +120,3 @@ if __name__ == "__main__":
     plt.scatter(x_values, y_values)
 
     plt.show()
-
-
